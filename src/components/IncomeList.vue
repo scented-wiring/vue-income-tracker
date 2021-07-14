81d1adc5601287eaec91f253cd7941eb40e8c8a9
Tom Hammersley <thomas.j.hammersley@gmail.com>
2021-07-14 18:23:59 +0100

    create IncomeList and IncomeItem components

---
+++ src/components/IncomeList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="income-list"></div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.income-list {
+  margin-top: 30px;
+  padding: 15px;
+}
+</style>
